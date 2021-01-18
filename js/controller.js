0a4cce81d78f7d511c68bc8f4205d3e2808475f0
Omi Karande <omkar.karande@outlook.com>
2021-01-18 14:45:00 -0800

    blanking commit

--- js/controller.js
+++
@@ -1,7 +0,0 @@
-var resumeApp = angular.module('resumeApp', ["ui.bootstrap"]);
-resumeApp.controller('resumeCtrl', function($scope, $http) {
-    $http.get('res/data/resume.json').success(function(data) {
-        $scope.resume_data = data;
-        $scope.previous = 0;
-    });
-});
